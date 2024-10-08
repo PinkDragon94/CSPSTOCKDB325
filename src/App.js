018f8315c4de314607aa8a5c7fef7e5267bf637c
Pinkdragon <cherandwhite@gmail.com>
2024-10-07 23:40:10 -0400

    1st commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Blog from './components/Blog';
+import Shop from './components/Shop';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Stock Predictions Dashboard</h1>
+      {/* Implement stock predictions, Bollinger Bands, buy/sell signals */}
+    </div>
+  );
+};
+
+export default Home;
